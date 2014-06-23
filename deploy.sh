14ec902293db0f50538e5331bb7d94fdecb331fb
Daniel <danielt69@gmail.com>
2014-06-23 21:30:33 +0300

    up and running

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,8 @@
 echo "startng pull and server restart" >> push.log
 date >> push.log
 cd /home/bitnami/apps/spree/htdocs
+sudo sh /home/bitnami/stack/ctlscript.sh stop apache
 git pull origin master && git push origin master #>> push.log
-sudo sh /home/bitnami/stack/ctlscript.sh restart apache #>> /home/bitnami/apps/gohub/push.log
+sudo sh /home/bitnami/stack/ctlscript.sh start apache #>> /home/bitnami/apps/gohub/push.log
 echo "end update at:" #>> push.log
 date #>> push.log
--- starting_script.sh
+++ starting_script.sh
@@ -3,8 +3,9 @@
 udo apt-get install golang
 
 # start the micro server on every startup:
+sudo crontab -e
 
-sudo -H -u bitnami bash -c 'go run gohub.go --config config.json --port 8392 --log gohub.log' 
+sudo -H -u bitnami bash -c 'go run /home/bitnami/mygohub/gohub.go --config config.json --port 8392 --log gohub.log' 
 
 # config your project github repo with webhook and you are done!!  more details on gohub project
 
